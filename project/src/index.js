16c5e113a1867e4b8b5f52ac0fb9d594e2e981d3
EnjoyTWOS <58084799+EnjoyTWOS@users.noreply.github.com>
2021-06-06 23:00:06 +0300

    Начинаем программировать (#3)
    * m1-t3
    
    * m1-t3

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  CARDS_ARRAY: [1, 2, 3, 4, 5],
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardsArray={Settings.CARDS_ARRAY}
+    />
   </React.StrictMode>,
   document.getElementById('root'));

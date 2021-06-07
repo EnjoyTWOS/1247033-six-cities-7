4da71a2c21e2a9670483a87248537bf0e23fe27f
EnjoyTWOS <58084799+EnjoyTWOS@users.noreply.github.com>
2021-06-07 03:31:59 +0300

    m2-t1

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import {AppRoute} from '../../const';
 import MainPage from '../main-page/main-page';
+import Favorites from '../favorites/favorites';
+import SignIn from '../sign-in/sign-in';
+import Room from '../room/room';
+import UnknownPage from '../unknown-page/unknown-page';
 
 function App(props) {
   const {cardsArray} = props;
 
   return (
-    <MainPage cardsArray={cardsArray} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <MainPage cardsArray={cardsArray} />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Room />
+        </Route>
+        <Route exact path={AppRoute.UnknownPage}>
+          <UnknownPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
